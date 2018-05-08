f9e582fc194eb413abf0a4aab6db5e92751a7fc8
childressdev <jcampbell@childressagency.com>
2018-05-08 15:12:34 -0400

    putting together ehader

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form action="<?php echo home_url(); ?>" method="get" class="navbar-form navbar-right">
+  <div class="input-group">
+    <input type="text" name="s" class="form-control" placeholder="Search" />
+    <span class="input-group-btn">
+      <button class="btn"  type="submit"><i class="fas fa-search"></i></button>
+    </span>
+  </div>
+</form>
