f1ad38fec2580457947df7b4818ebe8b1bded6f4
childressdev <jcampbell@childressagency.com>
2018-05-10 16:11:40 -0400

    changed to repeater field

--- partials/footer-stats.php
+++ partials/footer-stats.php
@@ -1,55 +1,22 @@
 <div id="stats" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <div class="row">
-        <div class="col-sm-3">
-          <p>Annual Miles Flown</p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('annual_miles_flown_year_1', 'option'); ?>: <span><?php the_field('annual_miles_flown_year_1_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('annual_miles_flown_year_2', 'option'); ?>: <span><?php the_field('annual_miles_flown_year_2_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('annual_miles_flown_year_3', 'option'); ?>: <span><?php the_field('annual_miles_flown_year_3_qty', 'option'); ?></span></p>
-        </div>
-      </div>
-    </div>
-
-    <div class="item">
-      <div class="row">
-        <div class="col-sm-3">
-          <p>Project Countries</p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('project_countries_year_1', 'option'); ?>: <span><?php the_field('project_countries_year_1_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('project_countries_year_2', 'option'); ?>: <span><?php the_field('project_countries_year_2_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('project_countries_year_3', 'option'); ?>: <span><?php the_field('project_countries_year_3_qty', 'option'); ?></span></p>
+    <?php if(have_rows('footer_stats', 'option')): $fs=0; while(have_rows('footer_stats', 'option')): the_row(); ?>
+      <div class="item<?php if($fs==0){ echo ' active'; } ?>">
+        <div class="row">
+          <div class="col-sm-3">
+            <p><?php the_sub_field('stat_title'); ?></p>
+          </div>
+          <div class="col-xs-4 col-sm-3">
+            <p><?php the_sub_field('stat_1_year'); ?>: <span><?php the_sub_field('stat_1_value'); ?></span></p>
+          </div>
+          <div class="col-xs-4 col-sm-3">
+            <p><?php the_sub_field('stat_2_year'); ?>: <span><?php the_sub_field('stat_2_value'); ?></span></p>
+          </div>
+          <div class="col-xs-4 col-sm-3">
+            <p><?php the_sub_field('stat_3_year'); ?>: <span><?php the_sub_field('stat_3_value'); ?></span></p>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="item">
-      <div class="row">
-        <div class="col-sm-3">
-          <p>Projects Complete</p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('projects_complete_year_1', 'option'); ?>: <span><?php the_field('projects_complete_year_1_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('projects_complete_year_2', 'option'); ?>: <span><?php the_field('projects_complete_year_2_qty', 'option'); ?></span></p>
-        </div>
-        <div class="col-xs-4 col-sm-3">
-          <p><?php the_field('projects_complete_year_3', 'option'); ?>: <span><?php the_field('projects_complete_year_3_qty', 'option'); ?></span></p>
-        </div>
-      </div>
-    </div>
-
+    <?php $fs++; endwhile; endif; ?>
   </div>
 </div>
