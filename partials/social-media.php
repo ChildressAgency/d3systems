5e3d2bdb0d65c2bfa0fbcff37ad14b7495e475af
childressdev <jcampbell@childressagency.com>
2018-05-24 14:58:59 -0400

    added target blank

--- partials/social-media.php
+++ partials/social-media.php
@@ -1,10 +1,10 @@
 <div class="social">
   <?php if(get_field('facebook', 'option')): ?>
-    <a href="<?php the_field('facebook', 'option'); ?>" class="facebook" title="Facebook"></a>
+    <a href="<?php the_field('facebook', 'option'); ?>" class="facebook" title="Facebook" target="_blank"></a>
   <?php endif; if(get_field('linkedin', 'option')): ?>
-    <a href="<?php the_field('linkedin', 'option'); ?>" class="linkedin" title="LinkedIn"></a>
+    <a href="<?php the_field('linkedin', 'option'); ?>" class="linkedin" title="LinkedIn" target="_blank"></a>
   <?php endif; if(get_field('newsletter', 'option')): ?>
-    <a href="<?php the_field('newsletter', 'option'); ?>" class="newsletter" title="Newsletter"></a>
+    <a href="<?php the_field('newsletter', 'option'); ?>" class="newsletter" title="Newsletter" target="_blank"></a>
   <?php endif; ?>
   <a href="<?php echo home_url('our-work'); ?>" class="d3" title="Our Work"></a>
 </div>
