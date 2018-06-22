d6463300479923b16ef8ca30d444ce906f0683c9
childressdev <jcampbell@childressagency.com>
2018-06-22 11:44:00 -0400

    added icon management

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
       <div class="row row-sm-height">
         <div class="col-sm-4 col-sm-height">
           <div id="designs-card" class="icon-card">
+            <img src="<?php the_field('designs_card_icon'); ?>" alt="" style="top:<?php the_field('designs_card_icon_top_position'); ?>px;" />
             <h2>Designs</h2>
             <?php the_field('designs_card_content'); ?>
             <a href="<?php the_field('designs_card_link'); ?>">Read More</a>
@@ -11,6 +12,7 @@
         </div>
         <div class="col-sm-4 col-sm-height">
           <div id="data-card" class="icon-card">
+            <img src="<?php the_field('data_card_icon'); ?>" alt="" style="top:<?php the_field('data_card_icon_top_position'); ?>px;" />
             <h2>Data</h2>
             <?php the_field('data_card_content'); ?>
             <a href="<?php the_field('data_card_link'); ?>">Read More</a>
@@ -18,6 +20,7 @@
         </div>
         <div class="col-sm-4 col-sm-height">
           <div id="decisions-card" class="icon-card">
+            <img src="<?php the_field('decisions_card_icon'); ?>" alt="" style="top:<?php the_field('decisions_card_icon_top_position'); ?>px;" />
             <h2>Decisions</h2>
             <?php the_field('decisions_card_content'); ?>
             <a href="<?php the_field('decisions_card_link'); ?>">Read More</a>
